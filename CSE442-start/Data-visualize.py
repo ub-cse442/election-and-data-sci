01844ab9677ab33305dedee08ec9a0d2be63fbb3
lethiraj <lethiraj@buffalo.edu>
2016-10-07 18:45:40 -0400

    graph testing

--- CSE442-start/Data-visualize.py
+++ CSE442-start/Data-visualize.py
@@ -2,29 +2,35 @@ import matplotlib.pyplot as plt
 import pandas
 import csv
 
+#author : lethiraj
 
-with open('2004.csv', newline='') as csvfile:
+def sun_data(file):
+    with open(file) as csvfile:
 
-    statelist=[]
-    republicanlist =[]
-    democratlist=[]
-    filereader = csv.reader(csvfile, delimiter=',')
+        statelist=[]
+        republicanlist =[]
+        democratlist=[]
+        filereader = csv.reader(csvfile, delimiter=',')
 
-    for line in filereader:
-
-
-        statelist.append(line[0])
-        democratlist.append(line[1])
-        republicanlist.append(line[2])
+        for line in filereader:
 
 
-del(statelist[0],democratlist[0],republicanlist[0])
+            statelist.append(line[0])
+            democratlist.append(line[1])
+            republicanlist.append(line[2])
 
-plt.figure(1)
-x = range(len(statelist))
-plt.xticks(x, statelist)
-plt.plot(x,democratlist,"b")
-plt.plot(x,republicanlist,"r")
-plt.xticks(range(0,len(statelist)))
-plt.show()
 
+    del(statelist[0],democratlist[0],republicanlist[0])
+
+    plt.figure(1)
+    x = range(len(statelist))
+    plt.xticks(x, statelist)
+    plt.plot(x,democratlist,"b")
+    plt.plot(x,republicanlist,"r")
+    plt.xticks(range(0,len(statelist)))
+    plt.show()
+    plt.savefig('rep_vs_dem_1.png')
+
+
+if __name__=="__main__":
+    sun_data('2000.csv')
