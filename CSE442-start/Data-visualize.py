28bd2cf4d9f7ed26d3b9591991e8e0dc005d26b2
lethiraj <lethiraj@buffalo.edu>
2016-10-09 12:20:46 -0400

    bar graphs

--- CSE442-start/Data-visualize.py
+++ CSE442-start/Data-visualize.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas
 import csv
+import numpy as np
 
 #author : lethiraj
 
@@ -22,16 +23,34 @@ def sun_data(file):
 
     del(statelist[0],democratlist[0],republicanlist[0])
 
-    plt.figure(1)
+    pos = range(0,len(statelist))
     x = range(len(statelist))
+
+    width = 0.25
+
     plt.xticks(x, statelist)
-    plt.plot(x,democratlist,"b",label = 'Democrat')
-    plt.plot(x,republicanlist,"r",label = 'Republican')
-    plt.xticks(range(0,len(statelist)))
-    plt.legend(loc = 'upper right')
-    plt.savefig('rep_vs_dem_2008.png')
+    plt.bar(x,democratlist,width,alpha=0.5,color='blue',label = 'Democrat')
+    plt.bar([p + width for p in pos],republicanlist,width,alpha=0.5,color='red',label='Republican')
+    plt.xticks(range(0, len(statelist)))
+    plt.ylabel('No of votes')
+    plt.xlabel('States')
+    plt.title('Democrat Vs Republican')
+    plt.legend(['Democrat', 'Republican'], loc='upper right')
+    plt.grid()
+    plt.savefig('rep_vs_dem_bar_2012.png')
+
     plt.show()
 
 
+
+
+
+
+
+
+
+
+
+
 if __name__=="__main__":
-    sun_data('2008.csv')
+    sun_data('2012.csv')
